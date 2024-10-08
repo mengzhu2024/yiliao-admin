ee60a3dd077fbf1851d9e8e552deb634fd90e012
huwenlong <17749687679@163.com>
2024-10-08 14:38:35 +0800

    first commit

---
+++ src/main/java/com/dao/JinjishiguDao.java
@@ -0,0 +1,30 @@
+package com.dao;
+
+import com.entity.JinjishiguEntity;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import java.util.List;
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.plugins.pagination.Pagination;
+
+import org.apache.ibatis.annotations.Param;
+import com.entity.vo.JinjishiguVO;
+import com.entity.view.JinjishiguView;
+
+
+/**
+ * 紧急事故
+ *
+ */
+public interface JinjishiguDao extends BaseMapper<JinjishiguEntity> {
+
+	List<JinjishiguVO> selectListVO(@Param("ew") Wrapper<JinjishiguEntity> wrapper);
+
+	JinjishiguVO selectVO(@Param("ew") Wrapper<JinjishiguEntity> wrapper);
+
+	List<JinjishiguView> selectListView(@Param("ew") Wrapper<JinjishiguEntity> wrapper);
+
+	List<JinjishiguView> selectListView(Pagination page,@Param("ew") Wrapper<JinjishiguEntity> wrapper);
+
+	JinjishiguView selectView(@Param("ew") Wrapper<JinjishiguEntity> wrapper);
+
+}
---
+++ src/main/java/com/dao/YiliaoyaopinDao.java
@@ -0,0 +1,30 @@
+package com.dao;
+
+import com.entity.YiliaoyaopinEntity;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import java.util.List;
+import com.baomidou.mybatisplus.mapper.Wrapper;
+import com.baomidou.mybatisplus.plugins.pagination.Pagination;
+
+import org.apache.ibatis.annotations.Param;
+import com.entity.vo.YiliaoyaopinVO;
+import com.entity.view.YiliaoyaopinView;
+
+
+/**
+ * 医疗药品
+ *
+ */
+public interface YiliaoyaopinDao extends BaseMapper<YiliaoyaopinEntity> {
+
+	List<YiliaoyaopinVO> selectListVO(@Param("ew") Wrapper<YiliaoyaopinEntity> wrapper);
+
+	YiliaoyaopinVO selectVO(@Param("ew") Wrapper<YiliaoyaopinEntity> wrapper);
+
+	List<YiliaoyaopinView> selectListView(@Param("ew") Wrapper<YiliaoyaopinEntity> wrapper);
+
+	List<YiliaoyaopinView> selectListView(Pagination page,@Param("ew") Wrapper<YiliaoyaopinEntity> wrapper);
+
+	YiliaoyaopinView selectView(@Param("ew") Wrapper<YiliaoyaopinEntity> wrapper);
+
+}
